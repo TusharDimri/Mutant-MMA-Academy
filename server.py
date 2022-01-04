da8c7a78e4918bdc42b21345ba52e2c3f77cd2a8
Tushar <tushar.dimri22@gmail.com>
2022-01-04 20:45:22 +0530

    Backend changes with gmail response

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+mail = Mail(app)
 
 
 @app.route("/")
@@ -12,8 +14,26 @@ def index():
 def contact():
     name = request.form.get('name')
     email = request.form.get('email')
+    phone = "+91" + str(request.form.get('phone'))
     query = request.form.get('query')
-    print(f"Name: {name}, Email: {email}, Query: {query}")
+    data = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nQuery: {query}\n"
+    print(data)
+
+    # Sending Mail from creater to owner:
+    app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = "tushar.dimri91@gmail.com"
+    app.config['MAIL_PASSWORD'] = "Tushar_8126"
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    mail = Mail(app)
+    msg = Message(
+        subject="Query from website", 
+        sender="tushar.dimri91@gmail.com",
+        recipients=["tushar.dimri22@gmail.com"]
+    )
+    msg.body = data
+    mail.send(msg)
     return redirect(url_for("index"))
 
 
