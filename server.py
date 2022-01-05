20f94f3e86226486dfc3c03e2c1bffe5e691981d
Sachin Rana <sachinrana1827@gmail.com>
2022-01-05 19:01:14 +0530

    added gitignore file

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 mail = Mail(app)
@@ -17,19 +21,18 @@ def contact():
     phone = "+91" + str(request.form.get('phone'))
     query = request.form.get('query')
     data = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nQuery: {query}\n"
-    print(data)
 
     # Sending Mail from creater to owner:
     app.config["MAIL_SERVER"] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = "tushar.dimri91@gmail.com"
-    app.config['MAIL_PASSWORD'] = "Tushar_8126"
+    app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     mail = Mail(app)
     msg = Message(
-        subject="Query from website", 
-        sender="tushar.dimri91@gmail.com",
+        subject="Query from website",
+        sender=os.getenv('EMAIL_USERNAME'),
         recipients=["tushar.dimri22@gmail.com"]
     )
     msg.body = data
